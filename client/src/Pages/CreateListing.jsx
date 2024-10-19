d532cca4c4507212bb6c2fed8c136c4b14a12227
SrajnaJ <jainsrajna@gmail.com>
2024-10-20 01:01:38 +0530

    add show more listings functionality

--- client/src/Pages/CreateListing.jsx
+++ client/src/Pages/CreateListing.jsx
@@ -155,7 +155,7 @@ export default function CreateListing() {
       if (data.success === false) {
         setError(data.message);
       }
-      navigate(`/listings/${data._id}`);
+      navigate(`/listing/${data._id}`);
     } catch (error) {
       setError(error.message);
       setLoading(false);
--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -16,7 +16,8 @@ export default function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  console.log(listings);
+  const [showMore, setShowMore] = useState(false);
+  // console.log(listings);
 
   // console.log(sidebarData);
   useEffect(() => {
@@ -51,9 +52,12 @@ export default function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if (data.length > 8) setShowMore(true);
+      else setShowMore(false);
       setListings(data);
       setLoading(false);
     };
@@ -107,6 +111,18 @@ export default function Search() {
     const searchQuery = urlParams.toString();
 
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("startIndex", startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) setShowMore(false);
+    setListings([...listings, ...data]);
   };
 
   return (
@@ -229,6 +245,15 @@ export default function Search() {
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
+
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className="text-green-700 hover:underline p-7 text-center w-full"
+            >
+              Show more..
+            </button>
+          )}
         </div>
       </div>
     </div>
