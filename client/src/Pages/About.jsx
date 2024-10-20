b5d1c9abb2bb56469302afca845aea0f980a0240
SrajnaJ <jainsrajna@gmail.com>
2024-10-20 14:11:44 +0530

    complete about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Estates Here
+      </h1>
+      <p className="mb-4 text-slate-700">
+        EstatesHere is a real estate platform designed to connect property
+        owners with buyers and renters. Whether you're selling, renting, or
+        searching for a property, EstatesHere simplifies the process by allowing
+        users to list properties or search for them using various filters,
+        including property type, offer type: sale or rent, parking availability,
+        and furnished status. While financial transactions are not handled on
+        the platform, users can easily get in touch with the property owners
+        directly through the listings, making communication smooth and
+        straightforward.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At EstatesHere, we prioritize convenience and transparency. With
+        intuitive filtering options, users can find properties that match their
+        exact needs—whether it's a furnished apartment with parking or a home in
+        a specific area. Property seekers can directly contact owners, fostering
+        a direct and personal connection between parties. Our goal is to make
+        property searches and listings as stress-free as possible, offering a
+        trusted platform for all your real estate needs.
+      </p>
+    </div>
+  );
 }
